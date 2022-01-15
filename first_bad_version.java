80d6a726953c307d5120ea20d2566e600d377341
Heaven Ascension Dio <45495374+Shidhanta@users.noreply.github.com>
2022-01-15 16:11:55 +0530

    Create first_bad_version.java

---
+++ first_bad_version.java
@@ -0,0 +1,23 @@
+//https://leetcode.com/problems/first-bad-version/submissions/
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int lo = 1;
+        int hi = n; 
+        while(lo<=hi)
+        {
+            int mid = lo + (hi-lo)/2;
+            if(isBadVersion(mid))
+            {
+                hi=mid-1;
+                
+            }
+            else
+            {
+                lo=mid+1;
+            }
+        }
+        
+        return lo;
+    }
+}
